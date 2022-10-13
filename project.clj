5e4bd87747c6177236f285ff9efb1ac369cd4d78
Marcus Crestani <marcus.crestani@active-group.de>
2022-10-13 11:16:06 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject demonad "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Example for active.clojure.monad."
+  :url "https://github.com/active-group/active-clojure-monad-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [active-clojure "0.23.0"]
-                 [org.clojure/java.jdbc "0.7.8"]
-                 [com.h2database/h2 "1.4.197"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [de.active-group/active-clojure "0.41.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [com.h2database/h2 "1.4.200"]])
